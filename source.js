a86f4362df7f672f9332b8df9d42a7f45bee6e95
David Cramer <dcramer@gmail.com>
2012-01-02 15:43:37 -0800

    Look up the dom everytime, because it doesnt matter and if it were to change we'd loe it

--- source.js
+++ source.js
@@ -1,20 +1,12 @@
-var awesome = function(domId) {
-  var elem = $('#' + domId)
-    , evt  = document.createEvent("MouseEvents");
+var awesome = function() {
+  var domId = $('a[style*=left\\:\\ 370px]')[0].id;
+  if (!domId) return;
+  var elem = $('#' + domId);
+  var evt  = document.createEvent("MouseEvents");
   evt.initMouseEvent("click", true, true, window,1, 0, 0, elem.offset().left, elem.offset().top, false, false, false, false, 0, null);
-  document.getElementById(domId).dispatchEvent(evt)
+  document.getElementById(domId).dispatchEvent(evt);
 };
 
-try {
-  awesome.domId = $('a[style*=left\\:\\ 370px]')[0].id;
-} catch(e) {
-  console.log("Could not automatically detect awesome button's DOM ID. Caught error: " + e);
-}
-
-if(typeof awesome.domId == 'undefined') {
-  awesome.domId = window.prompt("What's your awesome button's DOM ID?", "");
-}
-
-setInterval('awesome("'+awesome.domId+'")', 45000);
+setInterval('awesome()', 45000);
 awesome(awesome.domId);
-window.alert("I don't always awesome, but when I do, I do it automatically.")
+window.alert("I don't always awesome, but when I do, I do it automatically.");
