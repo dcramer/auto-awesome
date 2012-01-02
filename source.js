4438daed4f5d166f5eff2005d3c9dde63e42ce10
David Cramer <dcramer@gmail.com>
2012-01-02 15:47:54 -0800

    Support console logging

--- source.js
+++ source.js
@@ -1,6 +1,9 @@
 var awesome = function() {
   var domId = $('a[style*=left\\:\\ 370px]')[0].id;
   if (!domId) return;
+  if (console) {
+    console.log('Auto awesoming! (domId=' + domId + ')');
+  }
   var elem = $('#' + domId);
   var evt  = document.createEvent("MouseEvents");
   evt.initMouseEvent("click", true, true, window,1, 0, 0, elem.offset().left, elem.offset().top, false, false, false, false, 0, null);
